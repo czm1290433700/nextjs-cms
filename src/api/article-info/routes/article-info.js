3accc23e79409d251ce7689f6740a43471f362d8
chenzhenmin <1290433700@qq.com>
2022-08-20 12:15:29 +0800

    feat: 更新分页相关接口

---
+++ src/api/article-info/routes/article-info.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * article-info router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::article-info.article-info');
---
+++ src/api/article-info/services/article-info.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * article-info service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::article-info.article-info');
